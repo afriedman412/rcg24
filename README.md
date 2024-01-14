2d54363cb060fd2d917fb6e5fcf4ac40f1b6ed5f
wault <wolf@waults-Mac-mini.local>
2024-01-13 21:54:12 -0500

    adding gunicorn

--- README.md
+++ README.md
@@ -3,6 +3,3 @@ why is test_chart updating with 101 songs instead of 50???
 finish testing update_chart
 
 artist adding is unoptimized (does one at a time) but fine for now
-
-
-# MYSQL_DB = "rcg_mysql3_db"
--- rcg/src/db.py
+++ rcg/src/db.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
 
 
-def make_sql_connection() -> Connection[Any]:
+def make_sql_connection() -> Connection:
     if "MYSQL_PW" in os.environ:
         conn = connect(
             user=os.environ["MYSQL_USER"],
@@ -51,7 +51,7 @@ def make_sql_engine() -> Engine:
 
 def db_query(
     q: str,
-    conn: Union[Connection[Any], None] = None,
+    conn: Union[Connection, None] = None,
     commit: bool = False,
     close: bool = True
 ) -> Any:
